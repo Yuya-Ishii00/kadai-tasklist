240dd12060d797e0432a973427a879f16127b775
Yuya Ishii <yu00ya.0125@gmail.com>
2018-03-01 13:50:22 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    @tasks = current_user.tasks.page(params[:page]).per(3)
   end
 
   def show
@@ -13,14 +14,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      render 'new'
     end
   end
 
@@ -39,9 +40,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -53,4 +53,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasklists.build  # form_for 用
+      @tasks = current_user.tasklists.order('created_at DESC').page(params[:page])
+    end
   end
 end
